a37d96736527355708e9629d41fbf0e00d1ae6fd
Nick Hopewell <46057507+NHopewell@users.noreply.github.com>
2019-11-10 15:23:29 -0500

    Update mario_difficult_version.c

--- week_1/mario_difficult_version.c
+++ week_1/mario_difficult_version.c
@@ -4,11 +4,11 @@
 int main(void)
 {
 	int pyramid_height;
-		
+
 		do{
 			pyramid_height = get_int("How tall should the pyramid be? (pick a number less than 9)\n");
-			} while( pyramid_height<1 || pyramid_height>8 );
-			
+		} while( pyramid_height<1 || pyramid_height>8 );
+
 		int count = 1;
 		while(count <= pyramid_height){
 			for(int i=pyramid_height-count; i>0; i--){
