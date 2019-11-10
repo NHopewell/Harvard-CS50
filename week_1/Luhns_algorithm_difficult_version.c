7917a010dc11c31835c29a6bad751189cd8492bd
Nick Hopewell <46057507+NHopewell@users.noreply.github.com>
2019-11-10 18:19:14 -0500

    Update Luhns_algorithm_difficult_version.c

--- week_1/Luhns_algorithm_difficult_version.c
+++ week_1/Luhns_algorithm_difficult_version.c
@@ -18,13 +18,13 @@ int main(void)
     int sum_rest = 0;
     int total = 0;
     
-    for(int i=0; i<cc_number_length; i+=2){
+    for (int i = 0; i < cc_number_length; i +=2 ){
         int x = buffer[i] - '0';
-        x*=2;
-        if(x>9){
+        x *= 2;
+        if (x>9){
             char buffer_2 [10];
             snprintf(buffer_2, 10, "%d", x);
-            for(int j=0; j<2; j++){
+            for (int j = 0; j < 2; j++){
                 int y = buffer_2[j] - '0';
                 sum_every_other += y;
                 } 
@@ -32,15 +32,15 @@ int main(void)
                 sum_every_other += x;
         }
     }
-    for(int i=cc_number_length-1; i>0; i-=2){
+    for (int i = cc_number_length-1; i > 0; i-=2){
         int z = buffer[i] - '0';
         sum_rest += z;
     }
     
     total += sum_every_other + sum_rest;
     
-    if(total % 10 == 0){
-        if(cc_number_length == 15 && ( return_first_n_digits(cc_number, 2) ==34 
+    if (total % 10 == 0){
+        if (cc_number_length == 15 && ( return_first_n_digits(cc_number, 2) ==34 
                                       || return_first_n_digits(cc_number, 2) ==37)){
             printf("AMEX\n");
         } else if (cc_number_length == 16 && ( return_first_n_digits(cc_number, 2) == 51  
@@ -64,7 +64,7 @@ int main(void)
 int get_long_length(long number){
     // get the length of an a long
     int len = 1;
-    while(number>9){
+    while (number > 9){
         number /= 10;
         len ++;
     }
@@ -76,7 +76,7 @@ int return_first_n_digits(long number, int n_digits)
    // return first n_digits of a long
     int int_length = get_long_length(number);
     int counter = int_length-n_digits;
-    while(counter>0){
+    while (counter > 0){
         number/=10;
         counter--;
     }
