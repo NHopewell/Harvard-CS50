f51a707c5de1202987d4951ad689c58c86383b1e
Nick Hopewell <46057507+NHopewell@users.noreply.github.com>
2019-11-12 21:42:50 -0500

    Update mario_difficult_version.c

--- week_1/mario_difficult_version.c
+++ week_1/mario_difficult_version.c
@@ -19,27 +19,27 @@ int main(void)
 {
 	int pyramid_height;
 
-		do {    // prompt user for height 
-		        pyramid_height = get_int(
-				"How tall should the pyramid be? \
-				(pick a number less than 9)\n");
-		} while (pyramid_height < 1 || pyramid_height > 8);
+	do {    // prompt user for height 
+		pyramid_height = get_int(
+			"How tall should the pyramid be? \
+			(pick a number less than 9)\n");
+	} while (pyramid_height < 1 || pyramid_height > 8);
 
-		int count = 1;
-		while (count <= pyramid_height){
-			// print one less space than height, decrement
-			for (int i = pyramid_height-count; i > 0; i--){
-				printf(" ");
-				}
-			// print count hashes
-			for (int i = 1; i < count+1; i++){
-				printf("#");
-				} printf("  "); // print 2 spaces
-			// print count hashes
-			for (int i = 1; i < count+1; i++){
-				printf("#");
-				} printf("\n"); // print newline
-		// increment count
-		count += 1;        
-		}
+	int count = 1;
+	while (count <= pyramid_height){
+		// print one less space than height, decrement
+		for (int i = pyramid_height-count; i > 0; i--){
+			printf(" ");
+			}
+		// print count hashes
+		for (int i = 1; i < count+1; i++){
+			printf("#");
+			} printf("  "); // print 2 spaces
+		// print count hashes
+		for (int i = 1; i < count+1; i++){
+			printf("#");
+			} printf("\n"); // print newline
+	// increment count
+	count += 1;        
+	}
 }
