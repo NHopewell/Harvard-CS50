2d1d635eb4f237ede7ea7bd39fce9bb58be44ff4
NHopewell <nicholashopewell@gmail.com>
2019-11-24 18:46:15 -0500

    fixed crack

--- week_2_Arrays/crack_difficult_version.c
+++ week_2_Arrays/crack_difficult_version.c
@@ -3,119 +3,46 @@
 #include <crypt.h>
 #include <string.h>
 
-int check_args(int c);
-char build_arr_1(string letters);
-string build_arr_2(string letters);
-string build_arr_3(string letters);
-string build_arr_4(string letters);
-
 int main(int argc, string argv[])
 {
-    int ck = check_args(argc);
-    
-    if (ck != 0) { 
-        exit(1); 
-    }
-    
-    string letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    
-    build_arr_1(letters);
-    build_arr_2(letters);
-    build_arr_3(letters);
-    build_arr_4(letters);
-    
-    string fail = "Could not crack this password";
-    printf("%s", fail);  
-    
-}
-
-int check_args(int c){
-    if (c < 2 || c > 2) {
-        string usage = "Usage: ./crack hash";
+    if (argc !=2 || argv[1] == NULL) {
+        string usage = "Usage: ./crack hash\n";
         printf("%s", usage);
-        return 1;
+        exit(1);
     }
-    return 0;
-}
-
-int check_hash(string hash, string encrypted){
     
-    int n = strlen(hash);
-    for (int i = 0; i < n; i++){
-        if (hash[i+2] != encrypted[i]){
-            return 0;
+    // 26 lowercase + 26 uppercase + \0 counts as 1 character = 53
+    const int NUM_LETTERS = 53;
+
+    // looping will halt at null bit - this is used to halt below
+    string all_letters = "\0abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    
+    char salt[3];         
+    char to_check[5] = "\0\0\0\0\0";            
+    //second char of user passed string into salt
+    strncpy(salt, argv[1], 2); 
+    salt[2] = '\0';
+
+    for (int m = 0; m < NUM_LETTERS; m++){
+        for (int l = 0; l < NUM_LETTERS; l++){
+            for (int k = 0; k < NUM_LETTERS; k++){
+                for (int j = 0; j < NUM_LETTERS; j++){
+                    for (int i = 1; i < NUM_LETTERS; i++){
+                        // first pass through all_letters, other positions held null
+                        to_check[0] = all_letters[i];  
+                        to_check[1] = all_letters[j]; 
+                        to_check[2] = all_letters[k];  
+                        to_check[3] = all_letters[l]; 
+                        to_check[4] = all_letters[m]; 
+                        if (strcmp(crypt(to_check, salt), argv[1]) == 0){
+                            printf("%s\n", to_check);
+                            return 0;
+                        }
+                    }
+                }
+            }
         }
     }
-    return 1;
-}
-
-char build_arr_1(string letters){
-    const int N_LETS = strlen(letters);
-    string to_encrypt[2];
-    
-    for (int i = 0; i < N_LETS; i++){
-         to_encrypt[0] = letters[i];
-         char encrypted = crypt(to_encrypt, 'test');
-         int check = check_hash(argv[1], encrypted);
-         
-         if (check == 1) {
-                return letters[i]; }}
-}
-
-string build_arr_2(string letters) {
-    const int N_LETS = strlen(letters);
-    string to_encrypt[3];
-    
-    for (int i = 0; i < N_LETS; i++){
-        to_encrypt[0] = letters[i]
-        for (int j = 0; j < N_LETS; j++){
-            to_encrypt[1] = letters[j]
-            
-            string encrypted = crypt(to_encrypt, 'test');
-            int check = check_hash(argv[1], encrypted);
-            
-            if (check == 1) {
-                   return to_encrypt; }}}
-}
-
-string build_arr_3(string letters) {
-    const int N_LETS = strlen(letters);
-    string to_encrypt[4];
-    
-    for (int i = 0; i < N_LETS; i++){
-        to_encrypt[0] = letters[i]
-        for (int j = 0; j < N_LETS; j++){
-            to_encrypt[1] = letters[j]
-            for (int k = 0; k < N_LETS; k++){
-                to_encrypt[2] = letters[k]
-               
-                string encrypted = crypt(to_encrypt, 'test');
-                int check = check_hash(argv[1], encrypted);
-                
-                if (check == 1) {
-                       return to_encrypt; }}}}
-
-}
-
-string build_arr_3(string letters) {
-    const int N_LETS = strlen(letters);
-    string to_encrypt[5];
-    
-    for (int i = 0; i < N_LETS; i++){
-        to_encrypt[0] = letters[i]
-        for (int j = 0; j < N_LETS; j++){
-            to_encrypt[1] = letters[j]
-            for (int k = 0; k < N_LETS; k++){
-                to_encrypt[2] = letters[k]
-                for (int l = 0; l < N_LETS; l++){
-                    to_encrypt[3] = letters[l]
-                        
-                    string encrypted = crypt(to_encrypt, 'test');
-                    int check = check_hash(argv[1], encrypted);
-                    
-                    if (check == 1) {
-                           return to_encrypt; }}}}}
-
 }
 
 
