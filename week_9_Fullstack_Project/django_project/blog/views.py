e39615d9a33f983974f1ea6d125a0bdaa103bd5c
Nick Hopewell <nicholashopewell@gmail.com>
2020-08-30 16:59:54 -0400

    adds base templates

--- week_9_Fullstack_Project/django_project/blog/views.py
+++ week_9_Fullstack_Project/django_project/blog/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render # looks in templated folder
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Nick',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'Aug 30, 2020'
+    },
+    {
+        'author': 'Jim',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'Aug 31, 2020'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
